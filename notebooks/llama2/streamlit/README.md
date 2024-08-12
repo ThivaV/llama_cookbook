0cde492983b4b5abeef843c339a66dc3554b20b2
thivav <thivaharan28@gmail.com>
2024-08-12 23:54:54 +0530

    llama2 with streamlit

---
+++ notebooks/llama2/streamlit/README.md
@@ -0,0 +1,5 @@
+# Llama2-7b
+
+> Blogger with `llama2-7b`, `langchain` & `streamlit`
+
+![Blogger](../../../docs/img/llama2-7b_blogger.png)
--- notebooks/llama2/streamlit/app.py
+++ notebooks/llama2/streamlit/app.py
@@ -1,8 +1,38 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
-from langchain.llms import ctransformers
+from langchain_community.llms import CTransformers  # type: ignore
+from langchain_core.prompts import PromptTemplate   # type: ignore
 
 # talk to llama2 model
+def load_model():
+    """load model"""
+
+    llm = CTransformers(model='../../../models/llama-2-7b-chat.Q8_0.gguf', 
+                        model_type='llama', 
+                        config={"max_new_tokens": 256, "temperature": 0})
+    return llm
+
+
+# request for blog content
+def generate_blog(query, word_count, genre):
+    """generate blog"""
+
+    template = """
+    As a you a blogger, please write a blog for the topic {query} and under gener {genre}.
+    Please write the blog in {word_count} words.
+    Your blog strictly should be in markdown language.
+    Also, come with an appropriate title for the blog and set it at the top in bold letters.      
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["query", "word_count", "genre"], template=template
+    )
+
+    llama2_model = load_model()
+    response = llama2_model.invoke(
+        prompt.format(query=query, word_count=word_count, genre=genre)
+    )
+    return response
+
 
 def main():
     "main"
@@ -20,7 +50,8 @@ def main():
         "This application generates an :red[article] based on your :green[topic] and :blue[gener]. :sunglasses:"
     )
 
-    st.markdown("[check out the repository](https://github.com/ThivaV/llama_cookbook/tree/main/notebooks/llama2)")
+    st.markdown("[Check out the repository](https://github.com/ThivaV/llama_cookbook/tree/main/notebooks/llama2/streamlit)")
+    st.markdown("[Playground](https://huggingface.co/spaces/thivav/llama2-blogger?logs=container)")
 
     user_input = st.text_input("Enter the topic you wanted the blog")
 
